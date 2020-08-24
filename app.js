10614e1c47fab45968c8b2a5f1d0496a92658c21
joao Paulo <joaopaulozangrande@gmail.com>
2020-08-24 20:07:44 -0300

    change data by POST

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(function (req, res, next){
 
     form.parse(req, function (err, fields, files){
 
+      req.body = fields;
       req.fields = fields;
       req.files = files;
 
@@ -58,7 +59,6 @@ app.use(session({
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
