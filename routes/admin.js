8ff4da5f61ede67c00d879ee7be35a484d516893
joao Paulo <joaopaulozangrande@gmail.com>
2020-08-25 20:07:21 -0300

    reservations: create and update

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var users = require("./../inc/users");
 var admin = require("./../inc/admin");
 var menus = require("./../inc/menus");
+var reservations = require("./../inc/reservations");
 var router = express.Router();
 
 router.use(function (req, res, next){
@@ -129,6 +130,34 @@ router.delete("/menus/:id", function (req, res, next){
     }).catch(err=>{
 
        res.send(err);
+
+    });
+
+});
+
+router.post("/reservations", function (req, res, next){
+
+    reservations.save(req.fields, req.files).then(results=>{
+
+        res.send(results);
+
+    }).catch(err=>{
+
+        res.send(err);
+
+    });
+
+});
+
+router.delete("/reservations/:id", function (req, res, next){
+
+    reservations.delete(req.params.id).then(results=>{
+
+        res.send(results);
+
+    }).catch(err=>{
+
+        res.send(err);
 
     });
 
