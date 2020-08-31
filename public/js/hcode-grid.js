0ec838e196817debabf335a2c7c6601a549ea0c8
joao Paulo <joaopaulozangrande@gmail.com>
2020-08-31 18:58:31 -0300

    contact's admin

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -59,25 +59,33 @@ class HcodeGrid {
 
         this.formCreate = document.querySelector(this.options.formCreate);
 
-        this.formCreate.save({
-            success:()=>{
-                this.fireEvent('afterFormCreate');
-            },
-            failure:()=>{
-                this.fireEvent('afterFormCreateError');
-            }
-        });
+        if (this.formCreate){
 
-        this.formUpdate = document.querySelector(this.options.formUpdate);
+            this.formCreate.save({
+                success:()=>{
+                    this.fireEvent('afterFormCreate');
+                },
+                failure:()=>{
+                    this.fireEvent('afterFormCreateError');
+                }
+            });
 
-        this.formUpdate.save({
-            success:()=>{
-                this.fireEvent('afterFormUpdate');
-            },
-            failure:()=>{
-                this.fireEvent('afterFormUpdateError');
-            }
-        });
+        }
+
+        if (this.formUpdate){
+
+            this.formUpdate = document.querySelector(this.options.formUpdate);
+
+            this.formUpdate.save({
+                success:()=>{
+                    this.fireEvent('afterFormUpdate');
+                },
+                failure:()=>{
+                    this.fireEvent('afterFormUpdateError');
+                }
+            });
+
+        }
 
     }
 
